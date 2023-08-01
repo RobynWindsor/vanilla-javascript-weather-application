ac960f7418efb5cb1bd03b05faae5c29cb69716a
Robyn <127926809+Robyn-web@users.noreply.github.com>
2023-08-01 06:58:09 +0100

    Fixed SheCodes API Issues

--- src/script.js
+++ src/script.js
@@ -23,18 +23,19 @@ if (minutes < 10) {
 h3.innerHTML = `${day} ${hours}:${minutes}`;
 
 function search(city) {
-  let apiKey = "1d292957c6ee13304574a1bb47a59610";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  let apiKey = "748ed80fdo221bt48fa84019ab0b737f";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
   axios.get(apiUrl).then(showTemperature);
 }
 function showTemperature(response) {
   console.log(response.data.name);
-  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#city").innerHTML = response.data.city;
   document.querySelector("#temp").innerHTML = Math.round(
-    response.data.main.temp
+    response.data.temperature.current
   );
 
-  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#humidity").innerHTML =
+    response.data.temperature.humidity;
   document.querySelector("#wind").innerHTML = Math.round(
     response.data.wind.speed
   );
